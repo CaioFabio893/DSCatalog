b57c6bd19560091548f786de1f7386728b6fc232
Caio Fábio <caiofabio893@gmail.com>
2026-01-08 17:01:39 -0300

    busca por id

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -1,4 +1,10 @@
-package com.devsuperior.dscatalog.CategoryRepository;
+package com.devsuperior.dscatalog.repositories;
 
-public class repositories {
+
+import com.devsuperior.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
